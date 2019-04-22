296b0dcc47e4d79a088b4cd81e21f018df90feeb
AurorePaladin <aurorepaladin@gmail.com>
2019-04-22 19:10:46 +0800

    逻辑层 数组，最后一个就是当前页面

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -6,6 +6,9 @@ Page({
     logs: []
   },
   onLoad: function () {
+    console.log('---------------------')
+    console.log(getCurrentPages()[1] === this)
+    console.log('---------------------')
     this.setData({
       logs: (wx.getStorageSync('logs') || []).map(log => {
         return util.formatTime(new Date(log))
