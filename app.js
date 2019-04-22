14a2b8e41db95c909222c597fe279c4a82da3166
AurorePaladin <aurorepaladin@gmail.com>
2019-04-22 19:17:32 +0800

    逻辑层 支持CommonJS规范

--- app.js
+++ app.js
@@ -10,6 +10,13 @@ console.log('==============================')
 // Page 方法 用于定义页面对象
 // getApp 方法 用来获取全局应用程序对象
 // getCurrentPages 方法 用来获取当前页面的调用栈 （数组，最后一个就是当前页面）
+// wx 对象 用来提供核心API的
+// console.log(wx)
+
+// 3.小程序的JS是支持CommonJS规范的
+
+const foo = require('./utils/foo.js')
+foo.say('world')
 
 console.log('==============================')
 App({
